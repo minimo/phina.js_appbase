55794052a52bb48a2ae2793f10e7507db71872f8
minimo <minimo.mobile@gmail.com>
2015-09-09 23:59:10 +0900

    font

--- src/plugins/font.js
+++ src/plugins/font.js
@@ -69,51 +69,56 @@ phina.namespace(function() {
 });
 
 phina.namespace(function() {
-    phina.define("phina.asset.Font", {
-        superClass: "phina.asset.Asset",
+  phina.define("phina.asset.Font", {
+    superClass: "phina.asset.Asset",
 
-        init: function(path) {
-            this.superInit();
+    init: function(path) {
+      this.superInit();
 
-            var fontFaceStyleElement = tm.dom.Element("head").create("style");
-            fontFaceStyleElement.text = "@font-face { font-family: '{0}'; src: url({1}) format('{2}'); }".format(key, path, format);
+      var fontFaceStyleElement = tm.dom.Element("head").create("style");
+      fontFaceStyleElement.text = "@font-face { font-family: '{0}'; src: url({1}) format('{2}'); }".format(key, path, format);
+    },
 
-            tm.asset.Font.checkLoaded(key, function() {
-                this.flare("load");
-            }.bind(this));
-        },
-    });
+    _load: function(resolve) {
+      var self = this;
+      this.checkLoaded(key, function() {
+        self.loaded = true;
+        resolve(self);
+      }.bind(this));
+    },
 
-    tm.asset.Font.checkLoaded = function (font, callback) {
-        var canvas = tm.graphics.Canvas();
-        var DEFAULT_FONT = canvas.context.font.split(' ')[1];
-        canvas.context.font = '40px ' + DEFAULT_FONT;
+    checkLoaded = function (font, callback) {
+      var canvas = tm.graphics.Canvas();
+      var DEFAULT_FONT = canvas.context.font.split(' ')[1];
+      canvas.context.font = '40px ' + DEFAULT_FONT;
 
-        var checkText = "1234567890-^\\qwertyuiop@[asdfghjkl;:]zxcvbnm,./\!\"#$%&'()=~|QWERTYUIOP`{ASDFGHJKL+*}ZXCVBNM<>?_１２３４５６７８９０－＾￥ｑｗｅｒｔｙｕｉｏｐａｓｄｆｇｈｊｋｌｚｘｃｖｂｎｍ，．あいうかさたなをん時は金なり";
+      var checkText = "1234567890-^\\qwertyuiop@[asdfghjkl;:]zxcvbnm,./\!\"#$%&'()=~|QWERTYUIOP`{ASDFGHJKL+*}ZXCVBNM<>?_１２３４５６７８９０－＾￥ｑｗｅｒｔｙｕｉｏｐａｓｄｆｇｈｊｋｌｚｘｃｖｂｎｍ，．あいうかさたなをん時は金なり";
 
-        var before = canvas.context.measureText(checkText).width;
-        canvas.context.font = '40px ' + font + ', ' + DEFAULT_FONT;
+      var before = canvas.context.measureText(checkText).width;
+      canvas.context.font = '40px ' + font + ', ' + DEFAULT_FONT;
 
-        var checkLoadFont = function () {
-            if (canvas.context.measureText(checkText).width !== before) {
-                callback && callback();
-            } else {
-                setTimeout(checkLoadFont, 100);
-            }
-        };
+      var checkLoadFont = function () {
+        if (canvas.context.measureText(checkText).width !== before) {
+          callback && callback();
+        } else {
         setTimeout(checkLoadFont, 100);
-    };
-
-    tm.asset.Loader.register("ttf", function(path, key) {
-        return tm.asset.Font(path, key, "truetype");
-    });
-    tm.asset.Loader.register("otf", function(path, key) {
-        return tm.asset.Font(path, key, "opentype");
-    });
-    tm.asset.Loader.register("woff", function(path, key) {
-        return tm.asset.Font(path, key, "woff");
-    });
-    tm.asset.Loader.register("woff2", function(path, key) {
-        return tm.asset.Font(path, key, "woff2");
-    });
+        }
+      };
+      setTimeout(checkLoadFont, 100);
+    },
+  });
 });
+
+  tm.asset.Loader.register("ttf", function(path, key) {
+    return tm.asset.Font(path, key, "truetype");
+  });
+  tm.asset.Loader.register("otf", function(path, key) {
+    return tm.asset.Font(path, key, "opentype");
+  });
+  tm.asset.Loader.register("woff", function(path, key) {
+    return tm.asset.Font(path, key, "woff");
+  });
+  tm.asset.Loader.register("woff2", function(path, key) {
+    return tm.asset.Font(path, key, "woff2");
+  });
+
